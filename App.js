ccc2459305eacf08e146c89dcd022574d3f05f8a
GabrielBolduc <bolducGabriel@outlook.com>
2025-09-09 10:23:43 -0400

    Commit3

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Page1 from './Page1';
 import Page2 from './Page2';
 import Page3 from './Page3';
 export default function App() {
-  return <Page3 />;
+  return <Page1 />;
 }
 
 const styles = StyleSheet.create({
